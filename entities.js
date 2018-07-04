5145ba76afbc6052c3bbd72d60d094d4545733ac
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-07-04 12:51:36 -0500

    Update entities.js

--- entities.js
+++ entities.js
@@ -1,3 +1,31 @@
-class Bullet {
+const entityUpdate = () {
+	for(let i = 0; i < bullets.length; i++) {
+		if(bullets[i].dead !== true) {
+			if(bullets[i].xPos < 0 || bullets[i].yPos < 0 || bullets[i].xPos > 400 || bullets[i].yPos > 400) {
+				bullets[i].dead = true;
+			} else{
+				bullets[i].xPos += bullets[i].vX;
+				bullets[i].yPos += bullets[i].vY;
+			}
+		}
+	}
+	for(var i = 0; i <= bullets.length-1; i++) {
+		if(bullets[i].dead === true) {
+			bullets.splice(i,1);
+		}
+	}
+    }
+}
 
+
+
+class Bullet {
+	constructor(x,y,vx,vy) {
+		this.xPos = x;
+		this.yPos = y;
+		this.vX = vx;
+		this.vY = vy;
+		this.dead = false;
+		this.health = 5;
+	}
 }
