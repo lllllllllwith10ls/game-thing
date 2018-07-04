86af734efecc8840b3613a9491572401d4c2d02c
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-07-04 14:30:13 -0500

    Update entities.js

--- entities.js
+++ entities.js
@@ -6,6 +6,11 @@ const entityUpdate = () => {
 			} else{
 				bullets[i].xPos += bullets[i].vX;
 				bullets[i].yPos += bullets[i].vY;
+				for(let j = 0; j < enemies.length; j++) {
+					if(enemies[j].dead !== true) {
+						bulletCollision(i,j);
+					}
+				}
 			}
 		}
 	}
@@ -14,9 +19,87 @@ const entityUpdate = () => {
 			bullets.splice(i,1);
 		}
 	}
+	for(let i = 0; i < enemies.length; i++) {
+		if(enemies[i].dead !== true) {
+			if(enemies[i].vY < -enemies[i].speed) {
+				enemies[i].vY = -enemies[i].speed;
+			}
+			if(enemies[i].vX < -enemies[i].speed) {
+				enemies[i].vX = -enemies[i].speed;
+			}
+			if(enemies[i].vY > enemies[i].speed) {
+				enemies[i].vY = enemies[i].speed;
+			}
+			if(enemies[i].vX < enemies[i].speed) {
+				enemies[i].vX = enemies[i].speed;
+			}
+			if (enemies[i].xPos < 0) {
+				enemies[i].xPos = 0;
+				enemies[i].vX = -enemies[i].vX * 4/5;
+			}
+			if (enemies[i].xPos > 400) {
+				enemies[i].xPos = 400;
+				enemies[i].vX = -enemies[i].vX * 4/5;
+			}
+			if (enemies[i].yPos < 0) {
+				enemies[i].yPos = 0;
+				enemies[i].vY = -enemies[i].vY * 4/5;
+			}
+			if (enemies[i].yPos > 400) {
+				enemies[i].yPos = 400;
+				enemies[i].vY = -enemies[i].vY * 4/5;
+			}
+			if(enemies[i].behavior === "chaser") {
+				let angle = Math.atan2(enemies[i].xPos-player.xPos,enemies[i].yPos-player.yPos);
+				enemies[i].vX += -Math.sin(angle);
+				enemies[i].vY += -Math.cos(angle);
+			}
+				
+			enemies[i].xPos += enemies[i].vX;
+			enemies[i].yPos += enemies[i].vY;
+			for(let j = 0; j < enemies.length; j++) {
+				if(enemies[j].dead !== true && j !== i) {
+					enemyCollision(i,j);
+				}
+			}
+		}
+	}
+	for(var i = 0; i <= bullets.length-1; i++) {
+		if(bullets[i].dead === true) {
+			bullets.splice(i,1);
+		}
+	}
+	for(var i = 0; i <= enemies.length-1; i++) {
+		if(enemies[i].dead === true) {
+			enemies.splice(i,1);
+		}
+	}
+	
 };
 
-
+const bulletCollision = (b,e) => {
+	if((bullets[b].xPos-bullets[b].hitbox < enemies[e].xPos+enemies[e].hitbox) || (bullets[b].xPos+bullets[b].hitbox > enemies[e].xPos-enemies[e].hitbox) || (bullets[b].yPos-bullets[b].hitbox < enemies[e].yPos+enemies[e].hitbox) || (bullets[b].yPos+bullets[b].hitbox > enemies[e].yPos-enemies[e].hitbox)) {
+		if(bullets[b].health > enemies[b].health) {
+			bullets[b].health -= enemies[e].health;
+			enemies[e].dead = true;
+		} else {
+			enemies[e].health -= bullets[b].health;
+			bullets[b].dead = true;
+			if(enemies[e].health <= 0) {
+				enemies[e].dead = true;
+			}
+		}
+	}
+}
+const enemyCollision = (e1,e2) => {
+	if((enemies[e1].xPos-enemies[e1].hitbox < enemies[e2].xPos+enemies[e2].hitbox) || (enemies[e1].xPos+enemies[e1].hitbox > enemies[e2].xPos-enemies[e2].hitbox) || (enemies[e1].yPos-enemies[e1].hitbox < enemies[e2].yPos+enemies[e2].hitbox) || (enemies[e1].yPos+enemies[e1].hitbox > enemies[e2].yPos-enemies[e2].hitbox)) {
+		let angle = Math.atan2(enemies[e1].xPos-enemies[e2].xPos,enemies[e1].yPos-enemies[e2].yPos);
+		enemies[e1].vX += -Math.sin(angle);
+		enemies[e1].vY += -Math.cos(angle);
+		enemies[e2].vX += Math.sin(angle);
+		enemies[e2].vY += Math.cos(angle);
+	}
+}
 
 class Bullet {
 	constructor(x,y,vx,vy) {
@@ -25,6 +108,22 @@ class Bullet {
 		this.vX = vx;
 		this.vY = vy;
 		this.dead = false;
-		this.health = 5;
+		this.health = player.damage;
+		this.hitbox = 2.5;
 	}
 };
+class Crasher {
+	constructor(x,y,vx,vy) {
+		this.behavior = "chaser";
+		this.xPos = x;
+		this.yPos = y;
+		this.vX = vx;
+		this.vY = vy;
+		this.dead = false;
+		this.accel = 0.1;
+		this.speed = 3;
+		this.health = 2.5;
+		rhis.hitbox = 5;
+	}
+};
+
